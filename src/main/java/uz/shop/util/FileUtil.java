627d451e998d581841a5973a8330e5c515304cf6
Javokhir Ibrokhimov <javohiribrohimov0521@gmail.com>
2025-06-22 18:25:53 +0500

    yaxshi

--- src/main/java/uz/shop/util/FileUtil.java
+++ src/main/java/uz/shop/util/FileUtil.java
@@ -1,23 +1,5 @@
 package uz.shop.util;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Getter;
-import lombok.SneakyThrows;
-
-import java.io.File;
-import java.util.List;
-
-public class FileUtil<T> {
-    private static final ObjectMapper objectMapper =new ObjectMapper();
-
-    @SneakyThrows
-    public static <T> void write(File file, List<T> t){
-        objectMapper.writerWithDefaultPrettyPrinter().writeValue(file,t);
-    }
-
-    @SneakyThrows
-    public static <T> List<T> read(File file){
-        return objectMapper.readValue( file, new TypeReference<>() {});
-    }
+public class FileUtil {
+    
 }
